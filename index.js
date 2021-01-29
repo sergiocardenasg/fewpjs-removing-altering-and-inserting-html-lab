632ea22ed6a3524b448e06ee54a84257d45b4328
sergiocardenasg <sergiocardenas@utexas.edu>
2021-01-28 21:32:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Sergio is the champion';
